32fba10748f76f7eee548d14fa690970642adfa8
mzehnich <mzehnich@punyhuman.com>
2019-03-13 12:51:12 -0400

    added auto mode

--- degen/__init__.py
+++ degen/__init__.py
@@ -18,6 +18,7 @@ def create_parser():
 	parser.add_argument(
 		'file',
 		type = str,
+		nargs = '?', # optional to allow for auto mode
 		help = 'Input video file to process'
 	)
 	parser.add_argument(
@@ -45,8 +46,17 @@ def create_parser():
 		action='store_true',
 		help = 'Disable h264 video compression pass'
 	)
+	parser.add_argument(
+		'-auto',
+		'-a',
+		action='store_true',
+		help = "Automatic mode (look for and process clips based on filename, i.e. "
+			   "'degen-11-20-vidname.mp4' would try to find and process 'vidname.mp4' "
+			   "with a start time of 11 and end time of 20, and output 'vidname_auto.mp4')"
+	)
 	return parser
 
+# use ffprobe subprocess to find length of given video
 def getLengthInSeconds(filename):
 	result = subprocess.Popen(['ffprobe', filename], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 	linesFound = [x for x in result.stdout.readlines() if "Duration".encode() in x] #list comprehension - equiv to: for x in readlines, if duration in x, return x (i.e. add to return list)
@@ -57,19 +67,25 @@ def getLengthInSeconds(filename):
 	timeList = fullDurationStr.split("Duration: ")[1].split(".")
 	return sum(x * int(t) for x, t in zip([3600, 60, 1], timeList[0].split(":"))) + (float(timeList[1]) * 0.01)
 
-def getAvailableFilename(fileExt):
+# finds the first available filename given desired name and extension, appending a number at end of name until one doesn't exist
+def getAvailableFilename(filename, fileExt):
 	num = 0
-	fileStr = 'output'
+	fileStr = filename
 	while(os.path.isfile('./' + fileStr + '.' + fileExt) == True): # loop until we find one that doesn't exist
 		num += 1
-		fileStr = 'output' + str(num)
+		fileStr = filename + str(num)
 	return fileStr
 
-def processVideo(args):
-	process = subprocess.Popen(args, stdout=subprocess.PIPE)
-	process.communicate()
-	exitcode = process.wait()
-	return exitcode
+def isVideoFile(file):
+	return any(i for i in [(file[-4:] == str) for str in ['.mp4', '.avi']] if i != -1)
+
+# checks if filename is valid, returning a bool,str tuple of whether it's valid and if it's invalid, which character invalidates it
+def isValidFilename(filename):
+	invalidchars = ['/','\\','?','%','*',':','|','"','<','>']
+	for char in invalidchars:
+		if char in filename:
+			return False, char
+	return True, '_'
 
 
 #print(os.getcwd()) #current working dir
@@ -120,6 +136,7 @@ def main():
 	)
 	
 	parser = create_parser()
+	outFileExt = 'mp4' # always export to .mp4
 
 	# check for ffmpeg in path
 	env = os.environ['PATH']
@@ -134,19 +151,30 @@ def main():
 
 	args = parser.parse_args()
 
-	# find filename from args
+	# check if the user included arguments to disable audio or compression
+	if args.na:
+		print('disable audio flag found')
+	if args.nc:
+		print('disable video compression flag found')
+
+	# if auto arg is set, go into auto mode and quit after it's done
+	if(args.auto):
+		__autoMode(args, outFileExt)
+		quit()
+
+	# find file from args
 	if args.file == None or os.path.isfile(args.file) == False:
 		print('error: file not found')
 		quit()
 
-	filename = args.file
+	file = args.file
 
-	if not(any(i for i in [(filename[-4:] == str) for str in ['.mp4', '.avi']] if i != -1)):
+	# check if file extension matches any from our known list of valid video formats to process
+	if not(isVideoFile(file)):
 		print('error: file is not a valid video format, aborting')
 		quit()
 
-	outFileExt = 'mp4' # always export to .mp4, but as a reminder, use filename[-3:] to get the extension from filename var
-	inVideoLength = getLengthInSeconds(filename);
+	inVideoLength = getLengthInSeconds(file);
 
 	start = "" if args.start == None else args.start
 	end = "" if args.end == None else args.end
@@ -154,19 +182,18 @@ def main():
 	# prompt for a filename for the output video
 	name = ""
 	while(name == ""):
-		print('\nEnter clip name (blank defaults "output"):')
+		print('\nEnter clip name (without extension) (blank defaults "output"):')
 		name = input()
 		
 		if name == "":
-			name = getAvailableFilename(outFileExt)
+			name = getAvailableFilename('output', outFileExt)
 			print('warning: no name entered, defaulting to "' + name + '"')
 		else:
 			# check if name has any invalid filename characters
-			invalidchars = ['/','\\','?','%','*',':','|','"','<','>']
-			for char in invalidchars:
-				if char in name:
-					print('error: invalid character "', char,'" entered in filename')
-					name = ""
+			valid, char = isValidFilename(name)
+			if not valid:
+				print("error: invalid character '" + char + "' entered in filename")
+				name = ""
 
 	# make sure we found a start time
 	while(start == ""):
@@ -196,22 +223,52 @@ def main():
 			print('warning: end time entered is beyond video length, using end of video')
 			end = inVideoLength
 
-	duration = float(end) - float(start)
+	print('\n')
 
-	print('\nprocessing clip "' + filename + '" as "'+name+'.'+outFileExt+'", start:', start, 'end:', end, 'duration:', duration) 
+	__processVideo(file, outFileExt, start, end, args, name)
 
-	# check if the user included arguments to disable audio or compression
+
+# runs ffmpeg subprocess with args on given file
+def __processVideo(file, ext, start, end, args, outFilename):
+	# build our list of arguments to send to ffmpeg
 	includeaudio = not args.na
 	compressvideo = not args.nc
+	duration = float(end) - float(start)
 
-	# build our list of arguments to send to ffmpeg
-	input_args = ['ffmpeg', '-hide_banner', '-loglevel', 'warning', '-ss', str(start), '-i', filename]
+	input_args = ['ffmpeg', '-hide_banner', '-loglevel', 'error', '-ss', str(start), '-i', file]
 	vcodec_args = ['-c:v', 'h264', '-preset', 'fast', '-crf', '22'] if compressvideo else ['-c', 'copy', '-copyinkf'] #vcodec_args = ['-vcodec', 'h264']
 	acodec_args = ['-c:a', 'copy'] if includeaudio else ['-an']
-	output_args = ['-t', str(duration), name+'.'+outFileExt]
+	output_args = ['-t', str(duration), outFilename+'.'+ext]
 
+	print('processing clip "' + file + '" as "'+outFilename+'.'+ext+'", start:', start, 'end:', end, 'duration:', duration)
 	starttime = time.time()
-	exitcode = processVideo(input_args + vcodec_args + acodec_args + output_args)
-	timedelta = time.time() - starttime
 
-	print('finished processing video "'+name+'" with exit code', exitcode, 'in %.3f'%(timedelta)+'s')
+	process = subprocess.Popen(input_args + vcodec_args + acodec_args + output_args, stdout=subprocess.PIPE)
+	process.communicate()
+	exitcode = process.wait()
+
+	timedelta = time.time() - starttime
+	print('finished processing video "'+outFilename+'" with exit code', exitcode, 'in %.3f'%(timedelta)+'s')
+	return
+
+def __autoMode(args, ext):
+	# find all files that start with our auto syntax ("degen-##-##-") and end with valid video file type
+	print('automatic processing mode')
+	videoFiles = [file for file in os.listdir(os.getcwd()) if isVideoFile(file)]
+
+	# dict of filename:(starttime,endtime,outfilename)
+	filesToProcess = {}
+	for file in videoFiles:
+		if file.startswith('degen-'):
+			split = file.split('-')
+			if len(split) >= 4 and split[1].isdigit() and split[2].isdigit():
+				dashPositions = [pos for pos, char in enumerate(file) if char == '-'] # get indices of all dash characters in filename
+				sanitizedFile = file[dashPositions[2] + 1:] # filename stripped of auto mode text (using this instead of split[3] allows for hyphens in the filename)
+				print('found file: ' + sanitizedFile + ', start: ' + split[1] + ', end: ' + split[2])
+				filesToProcess[file] = split[1], split[2], sanitizedFile
+
+	for file, params in filesToProcess.items():
+		outName = params[2][:-4] + '_auto'
+		outExt = params[2][-3:]
+		outName = getAvailableFilename(outName, outExt)
+		__processVideo(file, ext, params[0], params[1], args, outName)
